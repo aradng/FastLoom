79096a8980e3008637fcee1737f719a3b5113744
Sepehr Solouki <sepehr@ethicadata.com>
2024-05-12 09:44:10 +0000

    Upgrade to py312

--- core_zenoa/date.py
+++ core_zenoa/date.py
@@ -1,20 +1,20 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import jdatetime
-import pytz
 
 
 def datetime_to_jalali(dt: datetime, date_only: bool = False) -> str:
-    dt = dt.astimezone(pytz.timezone("Asia/Tehran"))
+    dt = dt.astimezone(ZoneInfo("Asia/Tehran"))
     return jdatetime.datetime.fromgregorian(datetime=dt).strftime(
         "%Y/%m/%d" if date_only else "%Y/%m/%d - %H:%M"
     )
 
 
 def utcnow() -> datetime:
-    return datetime.now(pytz.utc)
+    return datetime.now(ZoneInfo('UTC'))
 
 
 def datetime_to_timestamp(value: datetime) -> int:
-    dt_utc = value.replace(tzinfo=pytz.UTC)
+    dt_utc = value.replace(tzinfo=ZoneInfo('UTC'))
     return int(dt_utc.timestamp())
