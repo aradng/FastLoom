318b182134d0e8edaafed227288a62258c289f5f
Sepehr Solouki <sepehr@ethicadata.com>
2024-06-13 19:09:41 +0000

    Add `get_rabbit_router`

--- core_zenoa/signals/depends.py
+++ core_zenoa/signals/depends.py
@@ -3,12 +3,18 @@ import os
 
 from faststream.confluent import KafkaBroker
 from faststream.confluent.fastapi import KafkaRouter
+from faststream.rabbit import RabbitBroker
+from faststream.rabbit.fastapi import RabbitRouter
 
 logger = logging.getLogger(__name__)
 
 
 def _get_kafka_url() -> str:
     return os.getenv("KAFKA_URL", "localhost:9092")
+
+
+def _get_rabbit_url():
+    return os.getenv("RABBIT_URL", "localhost:5672")
 
 
 def get_stream_broker(name: str, kafka_url: str | None = None) -> KafkaBroker:
@@ -21,5 +27,23 @@ def get_stream_broker(name: str, kafka_url: str | None = None) -> KafkaBroker:
 def get_stream_router(name: str, kafka_url: str | None = None) -> KafkaRouter:
     kafka_url = kafka_url or _get_kafka_url()
     router = KafkaRouter(kafka_url, schema_url=f"/asyncapi/{name}")
+    logger.debug(f"Created stream router: {name}: {router}")
+    return router
+
+
+def get_rabbit_broker(
+    name: str, rabbit_url: str | None = None
+) -> RabbitBroker:
+    rabbit_url = rabbit_url or _get_rabbit_url()
+    broker = RabbitBroker(rabbit_url)
+    logger.debug(f"Created stream broker: {name}: {broker}")
+    return broker
+
+
+def get_rabbit_router(
+    name: str, rabbit_url: str | None = None
+) -> RabbitRouter:
+    rabbit_url = rabbit_url or _get_rabbit_url()
+    router = RabbitRouter(rabbit_url, schema_url=f"/asyncapi/{name}")
     logger.debug(f"Created stream router: {name}: {router}")
     return router
