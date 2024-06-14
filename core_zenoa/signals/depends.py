8ecb85116368482cdddf05894091e7e4699598c5
Sepehr Solouki <sepehr@ethicadata.com>
2024-06-14 12:28:39 +0000

    Add faststream otel

--- core_zenoa/signals/depends.py
+++ core_zenoa/signals/depends.py
@@ -5,6 +5,8 @@ from faststream.confluent import KafkaBroker
 from faststream.confluent.fastapi import KafkaRouter
 from faststream.rabbit import RabbitBroker
 from faststream.rabbit.fastapi import RabbitRouter
+from faststream.rabbit.opentelemetry import RabbitTelemetryMiddleware
+from opentelemetry import trace
 
 logger = logging.getLogger(__name__)
 
@@ -35,7 +37,15 @@ def get_rabbit_broker(
     name: str, rabbit_url: str | None = None
 ) -> RabbitBroker:
     rabbit_url = rabbit_url or _get_rabbit_url()
-    broker = RabbitBroker(rabbit_url)
+    broker = RabbitBroker(
+        rabbit_url,
+        schema_url=f"/{name}/asyncapi",
+        middlewares=(
+            RabbitTelemetryMiddleware(
+                tracer_provider=trace.get_tracer_provider()
+            ),
+        ),
+    )
     logger.debug(f"Created stream broker: {name}: {broker}")
     return broker
 
@@ -44,6 +54,14 @@ def get_rabbit_router(
     name: str, rabbit_url: str | None = None
 ) -> RabbitRouter:
     rabbit_url = rabbit_url or _get_rabbit_url()
-    router = RabbitRouter(rabbit_url, schema_url=f"/{name}/asyncapi")
+    router = RabbitRouter(
+        rabbit_url,
+        schema_url=f"/{name}/asyncapi",
+        middlewares=(
+            RabbitTelemetryMiddleware(
+                tracer_provider=trace.get_tracer_provider()
+            ),
+        ),
+    )
     logger.debug(f"Created stream router: {name}: {router}")
     return router
