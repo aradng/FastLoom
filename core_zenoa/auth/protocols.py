6a4a7a75340b63525e6178398d7f85bb98207c95
Sepehr Solouki <sepehr@ethicadata.com>
2024-11-08 19:32:34 +0000

    Add BaseTenantSettings
    - Fixed `IAM_TOKEN_URL` type in `OAuth2Settings` protocol.

--- core_zenoa/auth/protocols.py
+++ core_zenoa/auth/protocols.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from typing import Protocol
 
 from pydantic import HttpUrl
 
 
 class OAuth2Settings(Protocol):
-    IAM_TOKEN_URL: HttpUrl
+    IAM_TOKEN_URL: Path
 
 
 class SidecarSettings(OAuth2Settings, Protocol):
---
+++ settings/base.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class IAMSettings(BaseModel):
+    IAM_SIDECAR_URL: HttpUrl = Field(
+        HttpUrl("http://iam:8000/api/iam/auth/sidecar")
+    )
+    IAM_TOKEN_URL: Path = Path("/api/iam/auth/login/basic")
+
+
+class TenantSettings(BaseModel):
+    name: str
+
+
+class TenantWithHostSettings(TenantSettings):
+    website_url: HttpUrl
+
+
+class MonitoringSettings(BaseModel):
+    PROJECT_NAME: str
+    ENVIRONMENT: str
+
+
+class BaseTenantSettings(IAMSettings, TenantSettings, MonitoringSettings): ...
+
+
+class BaseTenantWithHostSettings(
+    IAMSettings, TenantWithHostSettings, MonitoringSettings
+): ...
