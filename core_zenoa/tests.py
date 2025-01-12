e6847401e570010132898817d8bb9e1f92791b78
Sepehr Solouki <sepehr@ethicadata.com>
2025-01-12 15:52:41 +0000

    fix: test utils

--- core_zenoa/tests.py
+++ core_zenoa/tests.py
@@ -1,20 +1,33 @@
 import random
+import typing
 from pprint import pformat
 
-import bson
-from beanie import PydanticObjectId
 from deepdiff import DeepDiff
-from httpx import Response
+
+if typing.TYPE_CHECKING:
+
+    class ResponseType(typing.Protocol):
+        status_code: int
+        text: str
+        is_success: bool
 
 
 def assert_deep_diff(actual, expected, **options):
+    try:
+        import bson
+        from beanie import PydanticObjectId
+
+        options |= dict(
+            ignore_type_in_groups=[(bson.ObjectId, PydanticObjectId, str)]
+        )
+    except ImportError:
+        ...
     diff = DeepDiff(
         expected,
         actual,
         ignore_order=True,
         exclude_types={type(...)},  # ignore comparison with ellipsis
         verbose_level=2,
-        ignore_type_in_groups=[(bson.ObjectId, PydanticObjectId, str)],
         ignore_type_subclasses=False,
         ignore_nan_inequality=True,
         **options,
@@ -26,8 +39,12 @@ def random_mobile_number():
     return f"0912{random.randint(1000000, 9999999)}"
 
 
-def status_check(status: int, response: Response):
+def status_check(response: "ResponseType", status: int):
     assert response.status_code == status, (
         response.status_code,
         response.text,
     )
+
+
+def assert_success(response: "ResponseType"):
+    assert response.is_success, response.text
