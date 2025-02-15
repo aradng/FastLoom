775cb7ff05c233b334a0e2aed399fc3e78b6d0f1
Sepehr Solouki <sepehr@ethicadata.com>
2025-02-15 09:55:52 +0000

    fix: fix PhoneValidator typing

--- core_zenoa/types.py
+++ core_zenoa/types.py
@@ -1,7 +1,7 @@
 import re
 from typing import Annotated
 
-from pydantic import AfterValidator
+from pydantic import AfterValidator, BeforeValidator
 
 PHONE_REGEX = r"^(\+|00)\d{1,2}\s?((\(\d{3}\))|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}$"
 EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
@@ -9,13 +9,18 @@ EMAIL_REGEX = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 
 class PhoneValidation:
     @classmethod
-    def phone_validator(cls, string: str) -> str | None:
-        phone_regex = PHONE_REGEX
-        if re.match(phone_regex, string):
+    def phone_validator(cls, string: str | None) -> str | None:
+        if string and re.match(PHONE_REGEX, string):
             string = re.sub(r"^00", "+", string)
             string = re.sub(r"[()\s.-]", "", string)
             return string
         return None
+
+    @classmethod
+    def phone_validator_or_exc(cls, string: str) -> str:
+        if phone := cls.phone_validator(string):
+            return phone
+        raise ValueError("Invalid phone number")
 
 
 class EmailValidation:
@@ -27,7 +32,12 @@ class EmailValidation:
         return None
 
 
-PhoneNumber = Annotated[str, AfterValidator(PhoneValidation.phone_validator)]
+PhoneNumberOrNone = Annotated[
+    str | None, AfterValidator(PhoneValidation.phone_validator)
+]
+ValidatedPhoneNumber = Annotated[
+    str, BeforeValidator(PhoneValidation.phone_validator_or_exc)
+]
 
 
 def _national_id_validator(nc: str) -> str | None:
