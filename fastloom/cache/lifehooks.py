901c002fadb992b3aaa0025c56f39851cf532240
aradng <aradarang@gmail.com>
2025-12-16 18:17:26 +0330

    feat: redis guard gate

--- fastloom/cache/lifehooks.py
+++ fastloom/cache/lifehooks.py
@@ -7,30 +7,31 @@ from fastloom.meta import SelfSustaining
 _HAS_REDIS = False
 with suppress(ImportError):
     from aredis_om import get_redis_connection
-    from redis import Redis
+    from redis import Redis as SyncRedis
+    from redis.asyncio import Redis
     from redis.exceptions import ConnectionError
 
     _HAS_REDIS = True
 
 if TYPE_CHECKING:
-    from redis import Redis
+    from redis import Redis as SyncRedis
+    from redis.asyncio import Redis
 
 if not TYPE_CHECKING and not _HAS_REDIS:
+    SyncRedis = None
     Redis = None
 
 
 class RedisHandler(SelfSustaining):
     enabled: bool = False
     redis: Redis
-    sync_redis: Redis
+    sync_redis: SyncRedis
 
     def __init__(self, settings: RedisSettings) -> None:
         super().__init__()
         if not _HAS_REDIS:
             return
         self.redis = get_redis_connection(url=str(settings.REDIS_URL))
-        self.sync_redis = Redis.from_url(
-            url=str(settings.REDIS_URL), decode_responses=True
-        )
+        self.sync_redis = SyncRedis.from_url(url=str(settings.REDIS_URL))
         with suppress(ConnectionError):
             self.enabled = self.sync_redis.ping()
